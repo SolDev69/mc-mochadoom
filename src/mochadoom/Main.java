4317bf47b239cb47e9da8f556a94384f3be0c0fb
SolDev69 <40839581+SolDev69@users.noreply.github.com>
2022-12-09 08:58:18 -0500

    Update Main.java

--- src/mochadoom/Main.java
+++ src/mochadoom/Main.java
@@ -2,6 +2,10 @@ package mochadoom;
 
 public class Main {
   public static void main(String... args) {
-    Engine.main(new String[]{"-iwad", "./DOOM.WAD"});
+    try {
+      Engine.main(new String[]{"-iwad", "./DOOM.WAD"});
+    } catch (IOException e) {
+      System.out.println("Error loading DOOM.WAD!");
+      e.printStackTrace();
   }
 }
