a3f939e6a4c46513d43e148b00cfc43cd1a5557a
SolDev69 <40839581+SolDev69@users.noreply.github.com>
2022-12-09 08:56:51 -0500

    supposed to be strings

--- src/mochadoom/Main.java
+++ src/mochadoom/Main.java
@@ -2,6 +2,6 @@ package mochadoom;
 
 public class Main {
   public static void main(String... args) {
-    Engine.main(new String[]{-iwad, ./DOOM.WAD});
+    Engine.main(new String[]{"-iwad", "./DOOM.WAD"});
   }
 }
