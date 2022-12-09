e9fa9365b5cade9fdb11ba1b9d583b17cef0e05d
SolDev69 <40839581+SolDev69@users.noreply.github.com>
2022-12-09 08:55:03 -0500

    Update build-jar.sh

--- build-jar.sh
+++ build-jar.sh
@@ -2,7 +2,7 @@
 
 cd src
 find . -type f -name '*.class' -delete
-javac -cp . mochadoom/Engine.java
+javac -cp . mochadoom/Main.java
 jar cmf Manifest.txt mochadoom.jar .
 zip -d mochadoom.jar *.java README.md Manifest.txt
 
